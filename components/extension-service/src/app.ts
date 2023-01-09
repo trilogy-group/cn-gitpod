a3d32b66b08c05462f00feaf4c6f509958ea1050
Muhammad Bilal Afzal <bilal.afzal@trilogy.com>
2023-01-09 11:02:05 +0000

    feat: backup commit

---
+++ components/extension-service/src/app.ts
@@ -0,0 +1,34 @@
+import * as grpc from "@grpc/grpc-js";
+import {
+    ExtensionServiceService,
+    PreStartWorkspaceNotifyRequest,
+    PreStartWorkspaceNotifyResponse,
+} from "@cn-gitpod/extension-service/lib";
+
+const server = new grpc.Server();
+
+// ! Handler, move to new file
+const preStartWorkspaceNotifyHookHandler: grpc.handleUnaryCall<
+    PreStartWorkspaceNotifyRequest,
+    PreStartWorkspaceNotifyResponse
+> = (call, callback) => {
+    console.log(`extension-service server: preStartWorkspaceNotifyHookHandler`);
+    console.log("preStartWorkspaceNotifyHookHandler", call.request.toObject());
+
+    const response = new PreStartWorkspaceNotifyResponse();
+    callback(null, response);
+};
+
+// * adding services
+server.addService(ExtensionServiceService, {
+    preStartWorkspaceNotifyHook: preStartWorkspaceNotifyHookHandler,
+});
+
+server.bindAsync("0.0.0.0:8082", grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    server.start();
+    console.log(`🚀 Server listening on ${port}`);
+});
