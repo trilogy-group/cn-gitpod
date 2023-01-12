ba5a2df4a4b1ec7d8bdec2a46c01ac7b817587e4
Muhammad Bilal Afzal <bilal.afzal@trilogy.com>
2023-01-12 08:30:27 +0000

    feat: hookpoint 4 annotation added for testing

--- components/extension-service/src/hooks/postCreateWorkspacePodModifyHook.ts
+++ components/extension-service/src/hooks/postCreateWorkspacePodModifyHook.ts
@@ -13,19 +13,33 @@ import {
 const postCreateWorkspacePodModifyHookHandler: grpc.handleUnaryCall<
     PostCreateWorkspacePodModifyRequest,
     PostCreateWorkspacePodModifyResponse
-> = (call, callback) => {
+> = async (call, callback) => {
     console.log(`extension-service server: postCreateWorkspacePodModifyHookHandler`);
     console.log("postCreateWorkspacePodModifyHookHandler", call.request.toObject());
-
-    const request = call.request.toObject();
-    console.log(JSON.stringify(request));
     const response = new PostCreateWorkspacePodModifyResponse();
-
-    console.log(`point 1`);
     const pod = call.request.getPod();
-    console.log(`casting`);
+    const podMetadata = pod?.getMetadata();
+    const podMetadataAnnotations = podMetadata?.getAnnotationsMap();
+    const instanceId = call.request.getWorkspaceinstanceid();
+
+    // * check from prisma
+    const foundWSInstance = await prismaClient?.workspaceInstance.findUnique({
+        where: {
+            instanceId,
+        },
+    });
+    if (!foundWSInstance) {
+        console.log(`WS Instance not found in prisma with id: ${instanceId}`);
+        podMetadataAnnotations?.set("hookArch", "Hi i am x86");
+    } else {
+        console.log(`Found WS Instance in prisma with id: ${instanceId}`);
+        console.log(`arch is ${foundWSInstance?.arch}`);
+        podMetadataAnnotations?.set("hookArch", foundWSInstance?.arch === "arm" ? "Hi i am arm" : "Hi i am x86 x2");
+    }
+
+    pod?.setMetadata(podMetadata);
     response.setPod(pod);
-    console.log(`point 2`);
+    console.log(`Pod sent back!`);
     callback(null, response);
 };
 
