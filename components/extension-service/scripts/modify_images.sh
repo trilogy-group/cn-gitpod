1a17407023202475b0f9cb9d16cc5775e577a5b2
Kushal Chordiya <kushal.chordiya@trilogy.com>
2023-01-26 19:50:20 +0000

    scripts to deploy extension service

---
+++ components/extension-service/scripts/modify_images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ -z "$1" ]]
+then
+    echo "Provide namespace as a CLI argument"
+    exit 1
+fi
+SERVER_REPO="gitpod-infra-test/server"
+MANAGER_REPO="gitpod-infra-test/ws-manager"
+BUILDER_REPO="gitpod-infra-test/image-builder-mk3"
+
+SERVER_IMAGE_TAG=$(aws ecr describe-images --repository-name "$SERVER_REPO" --query "reverse(sort_by(imageDetails, &imagePushedAt))[:1]" | jq '.[]|.imageTags|.[]|select(contains("commit"))')
+MANAGER_IMAGE_TAG=$(aws ecr describe-images --repository-name "$MANAGER_REPO" --query "reverse(sort_by(imageDetails, &imagePushedAt))[:1]" | jq '.[]|.imageTags|.[]|select(contains("commit"))')
+BUILDER_IMAGE_TAG=$(aws ecr describe-images --repository-name "$BUILDER_REPO" --query "reverse(sort_by(imageDetails, &imagePushedAt))[:1]" | jq '.[]|.imageTags|.[]|select(contains("commit"))')
+
+kubectl set image -n "$1" deployment/server server=346945241475.dkr.ecr.us-east-1.amazonaws.com/"$SERVER_REPO":"$SERVER_IMAGE_TAG"
+kubectl set image -n "$1" deployment/ws-manager ws-manager=346945241475.dkr.ecr.us-east-1.amazonaws.com/"$MANAGER_REPO":"$MANAGER_IMAGE_TAG"
+kubectl set image -n "$1" deployment/image-builder-mk3 image-builder-mk3=346945241475.dkr.ecr.us-east-1.amazonaws.com/"$BUILDER_REPO":"$BUILDER_IMAGE_TAG"
