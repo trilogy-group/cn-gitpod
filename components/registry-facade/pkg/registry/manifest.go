d9500db692b0ea1e484c5d8133dd9b821f9a725f
viswak-cn <viswak.hanumanth@codenation.co.in>
2023-01-30 08:48:49 +0000

    Revert "add logs for tracing flow"
    This reverts commit 101a3801d03b6885a5eda3527dfec7a0c59516e2.

--- components/registry-facade/pkg/registry/manifest.go
+++ components/registry-facade/pkg/registry/manifest.go
@@ -106,9 +106,7 @@ func (mh *manifestHandler) getManifest(w http.ResponseWriter, r *http.Request) {
 	err := func() error {
 		log.WithFields(logFields).Debug("get manifest")
 		tracing.LogMessageSafe(span, "spec", mh.Spec)
-		// Devspaces-specific start
-		log.WithFields(logFields).Info("Request reached get manifest")
-		// Devspaces-specific end
+
 		var (
 			acceptType string
 			err        error
@@ -367,9 +365,6 @@ func AsFetcherFunc(f remotes.Fetcher) FetcherFunc {
 // DownloadManifest downloads and unmarshals the manifest of the given desc. If the desc points to manifest list
 // we choose the first manifest in that list.
 func DownloadManifest(ctx context.Context, fetch FetcherFunc, desc ociv1.Descriptor, options ...ManifestDownloadOption) (cfg *ociv1.Manifest, rdesc *ociv1.Descriptor, err error) {
-	// Devspaces-specific start
-	log.Info("Request reached Download Manifest func")
-	// Devspaces-specific end
 	var opts manifestDownloadOptions
 	for _, o := range options {
 		o(&opts)
@@ -440,9 +435,6 @@ func DownloadManifest(ctx context.Context, fetch FetcherFunc, desc ociv1.Descrip
 
 	switch rdesc.MediaType {
 	case images.MediaTypeDockerSchema2ManifestList, ociv1.MediaTypeImageIndex:
-		// Devspaces-specific start
-		log.Info("Request reached Download Manifest rdesc.MediaType switch")
-		// Devspaces-specific end
 		log.WithField("desc", rdesc).Debug("resolving image index")
 
 		// we received a manifest list which means we'll pick the default platform
