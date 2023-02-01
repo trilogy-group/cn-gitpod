5cfbd782e9a633fb935527fef2b41ae832f3c29f
viswak-cn <viswak.hanumanth@codenation.co.in>
2023-02-01 11:29:17 +0000

    add script for pushing image-index

---
+++ scripts/create-image-index-public.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+component=$1
+
+full_repo=public.ecr.aws/k1t8c0v2/bilal-cn-gp/"$component"
+arm_ref="$full_repo":arm64
+amd_ref="$full_repo":amd64
+
+
+docker pull "$arm_ref"
+docker pull "$amd_ref"
+
+docker manifest rm "$full_repo"
+docker manifest create "$full_repo" "$arm_ref" "$amd_ref" --amend
+docker manifest annotate --arch arm64 "$full_repo" "$arm_ref"
+docker manifest push "$full_repo"
