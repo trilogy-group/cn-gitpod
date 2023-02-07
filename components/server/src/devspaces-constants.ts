2591c2969eb0831423f96c493f1179964133f987
Muhammad Bilal Afzal <bilal.afzal@trilogy.com>
2023-02-07 20:41:36 +0000

    refactor: dummy commit

--- components/server/src/devspaces-constants.ts
+++ components/server/src/devspaces-constants.ts
@@ -4,4 +4,5 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
+// ! Define constants for Devspaces-specific related stuff
 export const IMAGE_ARCH_MISMATCH_ERROR = "DS: Image arch mismatch";
