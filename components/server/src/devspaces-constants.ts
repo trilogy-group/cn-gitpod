ec2a4b6799eb7823d3d5278285be59afac189b54
Muhammad Bilal Afzal <bilal.afzal@trilogy.com>
2023-02-23 13:27:22 +0000

    fix: arch mismatch fix

---
+++ components/server/src/devspaces-constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2023 Gitpod GmbH. All rights reserved.
+ * Licensed under the GNU Affero General Public License (AGPL).
+ * See License-AGPL.txt in the project root for license information.
+ */
+
+// ! Define constants for Devspaces-specific related stuff
+export const IMAGE_ARCH_MISMATCH_ERROR = "DS: Image arch mismatch";
